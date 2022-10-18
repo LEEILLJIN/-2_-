e1fa3a4113d4a295d38195cac3e935b738b371fe
racgooCplat <93126952+racgooCplat@users.noreply.github.com>
2022-10-18 23:02:27 +0900

    테스트 더미파일 삭제

--- IceBox_manage/product_show.py
+++ IceBox_manage/product_show.py
@@ -22,7 +22,7 @@ def load_json ():
         return json.load(f)
 
 def show_item(item):
-    print('bulk-for-unit' in item)
+    # print('bulk-for-unit' in item)
     if 'bulk-for-unit' in item:
         #unpackaged
         print("<상품 ID: {}, 상품명: {}, 총량: {}, 단위 수량: {}, 현재량: {}, 카테고리: {}, 분류: {}, 보관권장온도: {}, 유통기한: {}>" .format(item["ID"], item["name"], item["total-number"], item["bulk-for-unit"],item["leftover-number"], item["category"], item["partition"], item["recommended-temp"], item["expiration-date"]))
@@ -102,4 +102,4 @@ def product_show():
     
 # print(dic_key_change([{"test":1,"test2":2}],"test2","nextkey"))
 
-product_show()
+# product_show()
