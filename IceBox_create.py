01c93324e6a273ee807ea12a139cd0ec0469e3be
syaedisyafrilnizam <60510081+syaedii@users.noreply.github.com>
2022-10-18 20:21:58 +0900

    icebox_create fix

--- IceBox_create.py
+++ IceBox_create.py
@@ -8,6 +8,7 @@ def createIceBox(today):
     with open("./data/IceBox_data.json", 'r', encoding='UTF8') as file:
         json_data = json.load(file)
 
+    json_data['today'] = today
     iceBox = json_data['iceboxes']
 
     inputData = {
@@ -21,26 +22,28 @@ def createIceBox(today):
         while True:
             try:
                 i = float(input(f"{inputData[key]}를 입력해주세요. >> "))
-                iceBox[0][key] = i
+                inputData[key] = i
                 break
             except ValueError:
                 print("숫자를 입력하세요.")
 
-    iceBox[0]["creation-status"] = True
+    inputData["name"] = "home"
+    iceBox.append(inputData)
 
     #JSON 파일 업데이트
     with open("./data/IceBox_data.json", 'w', encoding='UTF8') as file:
         json.dump(json_data, file, ensure_ascii=False, indent=2)
 
-    # 냉장고 정보 출력
-    print("\n현재 냉장고")
-    sizeData = ["refrigerator-size","freezer-size"]
-    tempData = ["refrigerator-temp", "freezer-temp"]
-    partition = ["냉장", "냉동"]
-
-    for i in range(len(partition)):
-        print(f"{partition[i]} - <{iceBox[0][sizeData[i]]}L, {iceBox[0][tempData[i]]}°C>")
     print("")
+    # 냉장고 정보 출력
+    # print("\n현재 냉장고")
+    # sizeData = ["refrigerator-size","freezer-size"]
+    # tempData = ["refrigerator-temp", "freezer-temp"]
+    # partition = ["냉장", "냉동"]
+    #
+    # for i in range(len(partition)):
+    #     print(f"{partition[i]} - <{iceBox[0][sizeData[i]]}L, {iceBox[0][tempData[i]]}°C>")
+    # print("")
 
     # 메인 메뉴로 이동
     IceBox_menu.MainMenu(today)
