60720390cdb7ff91d3233aa0a120be02a2603bc9
LEEILLJIN <qjsro368@naver.com>
2022-10-21 17:37:39 +0900

    날짜 입력 수정(경계값, 공백처리), product_update에 수정된 json 파일 키 값 적용

--- IceBox.py
+++ IceBox.py
@@ -1,25 +1,33 @@
 #main (초기 화면)
 #기획서에는 json 파일을 냉장고 생성할 때 같이 만드는거로 되었지만 일단 예시 파일로 구현 진행
 import datetime
+import os
 
 import IceBox_menu
 
 
 def DateInput():
+    os.system("cls")
     while True:
         today = str(input("오늘 날짜를 입력해주세요. >> "))
         today = validate_date(today)
         if(type(today) == str):
-            print(today,"날짜 적합\n")
+            # print(today,"날짜 적합\n")
             IceBox_menu.MainMenu(today)
             #냉장고 생성 후 json파일이 만들어질떄 today data를 넣기위해 인자로 전달
         else:
-            print(today,"날짜 부적합")
+            print("유효하지 않은 날짜 입니다. 다시 입력해주세요.")
             continue
 
 def validate_date(today):
     try:
         temp = today.split("-")
+        year = int(temp[0])
+        month = int(temp[1])
+        day = int(temp[2])
+
+        if 2000 > year or 2999 < year:
+            return False
         for i in range(len(temp)):
             temp[i] = str(int(temp[i]))
         today='-'.join(temp)
